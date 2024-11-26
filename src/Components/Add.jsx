32cbd5b76d83f08a65ad1254fb692fbbeae344a6
farmdor00 <farmdor00@gmail.com>
2024-11-26 22:54:59 +0530

    Update Add.jsx

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -3,11 +3,15 @@ import HCaptcha from '@hcaptcha/react-hcaptcha';
 
 const Add = ({ closeAdd }) => {
     const [confess, setConfession] = useState('');
+    const [confessLength, setconfessLength] = useState(confess.length)
     const [isLoading, setIsLoading] = useState(false);
     const [hCaptchaToken, setHCaptchaToken] = useState('');
 
     const handleInputChange = (event) => {
-        setConfession(event.target.value);
+        if(event.target.value.length<=350){
+            setconfessLength(event.target.value.length)
+            setConfession(event.target.value);
+        }
     };
 
     const handleCaptchaVerification = (token) => {
@@ -55,7 +59,7 @@ const Add = ({ closeAdd }) => {
 
     return (
         <div className="flex flex-col items-center transition-all p-4 sm:p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto m-2 w-full sm:w-[400px] relative">
-            <p className="absolute left-3 top-2 text-lg" onClick={closeAdd}>
+            <p className="absolute left-3 top-2 text-lg cursor-pointer" onClick={closeAdd}>
                 &#10005;
             </p>
             <h5 className="mb-2 w-full text-xl sm:text-2xl font-bold text-gray-900 dark:text-white text-center border-b pb-2">
@@ -63,12 +67,14 @@ const Add = ({ closeAdd }) => {
             </h5>
             <textarea
                 id="message"
+                maxLength= '300'
                 rows="4"
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Write your thoughts here..."
                 value={confess}
                 onChange={handleInputChange}
             />
+            <p className='text-right w-full'>{confessLength}/300</p>
             <p>Even the developer will not know your identity. So, feel free to confess.</p>
 
             <HCaptcha
